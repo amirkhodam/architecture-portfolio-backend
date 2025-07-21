de58b08ed1f7fadefab1f89c8aabe4e02e967e1c
AmirKhodam <77395761+amirkhodam@users.noreply.github.com>
2025-07-21 15:53:31 +0330

    feat(app): configure RouterModule for API routes to manage portfolio endpoints

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppService } from './app.service';
 import { PortfolioModule } from './portfolio/portfolio.module';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
+import { RouterModule } from '@nestjs/core';
 
 @Module({
   imports: [
@@ -14,6 +15,17 @@ import { join } from 'path';
     }),
     AuthModule,
     PortfolioModule,
+    RouterModule.register([
+      {
+        path: 'api',
+        children: [
+          {
+            path: 'portfolios',
+            module: PortfolioModule,
+          },
+        ],
+      },
+    ]),
   ],
   controllers: [AppController],
   providers: [AppService],
