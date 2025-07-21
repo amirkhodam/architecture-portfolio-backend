20ee83e54f49fc76205194a280e6521d2de4bb17
AmirKhodam <77395761+amirkhodam@users.noreply.github.com>
2025-07-21 15:53:44 +0330

    feat(main): enable CORS in the bootstrap function for cross-origin requests

--- src/main.ts
+++ src/main.ts
@@ -4,5 +4,10 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
+  app.enableCors({
+    allowedHeaders: '*',
+    origin: '*',
+    credentials: true,
+  });
 }
 bootstrap();
